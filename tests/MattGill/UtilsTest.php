39b1c4abba933edfe378f107aedc09876ef2917e
Matt Gill <matt.gill@mention-me.com>
2020-05-04 09:42:15 +0100

    Add a test

--- tests/MattGill/UtilsTest.php
+++ tests/MattGill/UtilsTest.php
@@ -20,6 +20,20 @@ class UtilsTest extends TestCase
         );
     }
 
+    /**
+     * @dataProvider provideClassNameToShortClassName
+     *
+     * @param string $input
+     * @param string $expected
+     */
+    public function testGetShortClassName(string $input, string $expected): void
+    {
+        $this->assertSame(
+            $expected,
+            Utils::getShortClassName($input)
+        );
+    }
+
     public function provideClassNameToStageName(): array
     {
         return [
@@ -37,6 +51,20 @@ class UtilsTest extends TestCase
             ],
             // There will be loads of strings that don't work in the currrent implementation. These tests currently just
             // test the existing use of SomeClass::class being passed as an argument.
+        ];
+    }
+
+    public function provideClassNameToShortClassName(): array
+    {
+        return [
+            "Just one word" => [
+                "OneWord",
+                "OneWord"
+            ],
+            "Fully qualified class name" => [
+                "Slashes\\Between\\Things",
+                "Things",
+            ],
         ];
     }
 }
